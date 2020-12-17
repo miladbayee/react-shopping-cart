4126b103c63a245dab43d95b852b40f119b0c72f
milad bayee <miladbayee@gmail.com>
2020-12-17 23:01:43 +0330

    create procees section

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import formatCurrency from "../util";
 import Button from "./Button";
 
@@ -6,6 +7,14 @@ export default function Cart({
   handleRemoveItem,
   handleRemoveCartList,
 }) {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleShowForm = () => {
+    setShowForm(true);
+  };
+
+const handleProceesFormSubmit=()=>{}
+  
   return (
     <div className={"cart cart-contain"}>
       <div className={"cart-count"}>
@@ -28,12 +37,12 @@ export default function Cart({
           {cartItem.map((item) => (
             <li key={item._id}>
               <div className="cart-item_img">
-                <div className='cart-item_item'>
+                <div className="cart-item_item">
                   <img src={item.image} alt={item.title} />
                   <p>{item.title}</p>
                 </div>
-                <div className='cart-item_size'>
-                <p>size : {item.size}</p>
+                <div className="cart-item_size">
+                  <p>size : {item.size}</p>
                 </div>
               </div>
               <div className="cart-item_price">
@@ -59,9 +68,40 @@ export default function Cart({
               cartItem.reduce((acc, cur) => acc + cur.price * cur.count, 0)
             )}
           </p>
-          <Button btnClass={"cart-proceed_btn button-add"}>Proceed</Button>
+          <Button
+            btnClass={"cart-proceed_btn button-add"}
+            handleOnClick={() => {
+              handleShowForm();
+            }}
+          >
+            Proceed
+          </Button>
         </div>
       )}
+      {showForm && 
+      <div className='process'>
+        <form className='process-form' onSubmit={handleProceesFormSubmit}>
+          <ul>
+            <li>
+              <label htmlFor="">Name</label>
+            <input type="text" placeholder='insert your name'/>
+            </li>
+            <li>
+            <label htmlFor="">Email</label>
+            <input type="mail" placeholder='insert your mail'/>
+            </li>
+            <li>
+            <label htmlFor="">Address</label>
+            <input type="text" placeholder='insert your address'/>
+            </li>
+            <li>
+            <Button btnClass='button-add procees-btn'>Ckeckout</Button>
+            </li>
+          </ul>
+          
+        </form>
+      </div>
+      }
     </div>
   );
 }
